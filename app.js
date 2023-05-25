f37c8354ca6df19d06ba82678f92aaec5a197df9
Vijin Vinod <vfcvijin@gmail.com>
2023-05-25 23:49:55 +0530

    chat button and progress button added

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ userRoute.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public/admin")));
 
 mongoose.set("strictQuery", true);
-mongoose.connect("mongodb://127.0.0.1:27017", () =>
+mongoose.connect(process.env.MONGO_URL, () =>
   console.log("Database connection established")
 );
 
