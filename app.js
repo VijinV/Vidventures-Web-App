eb013625f1f6e8c14f67f989a83969027f463f5e
Vijin Vinod <vfcvijin@gmail.com>
2023-06-29 19:07:54 +0530

    jsh

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const cors = require('cors');
 // requiring config files
 require("dotenv").config();
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 
 const port = process.env.PORT || 4000
